55935426b2b27741deccd074bec60d5e23de3155
Lara Filippone <lara.filippone@gmail.com>
2025-07-08 19:55:12 +0200

    feat: create db engine

---
+++ src/services/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.core.settings import config
+
+
+engine = create_engine(f"{config.DB_CONNECTION}://{config.DB_USERNAME}:{config.DB_PASSWORD}@{config.DB_HOST}:"
+                       f"{config.DB_PORT}/{config.DB_DATABASE}")
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
