0f811b142e8c75f4929895e8a01da587a31457d8
Lara Filippone <lara.filippone@gmail.com>
2025-07-12 17:27:45 +0200

    fix database connection

--- src/services/database.py
+++ src/services/database.py
@@ -1,10 +1,7 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import create_engine
 
 from src.core.settings import config
 
 
 engine = create_engine(f"{config.DB_CONNECTION}://{config.DB_USERNAME}:{config.DB_PASSWORD}@{config.DB_HOST}:"
                        f"{config.DB_PORT}/{config.DB_DATABASE}")
-
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
