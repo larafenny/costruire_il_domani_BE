365f51aff8cf1585a99c6490adbdb4461bfef5cf
Lara Filippone <lara.filippone@gmail.com>
2025-07-13 11:22:10 +0200

    feat: add method to verify user password

--- src/services/jwt_service.py
+++ src/services/jwt_service.py
@@ -56,3 +56,6 @@ class JWTService:
         # Hashing the password
         return bcrypt.hashpw(bytes, salt).decode("utf-8")
 
+    @staticmethod
+    def verify_user_password(plain_password: str, hashed_password: str) -> bool:
+        return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
