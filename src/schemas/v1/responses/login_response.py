fa8f1747cf9362d0dae066271c94bacc68aa1cd4
Lara Filippone <lara.filippone@gmail.com>
2025-07-13 11:21:52 +0200

    feat: add login request and response

---
+++ src/schemas/v1/responses/login_response.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field, model_validator
+from typing import Optional
+
+
+class LoginResponse(BaseModel):
+    response_code: int = Field(
+        ...,
+        ge=200,
+        le=600,
+        description="Response code of the request"
+    )
+    message: Optional[str] = Field(
+        description="Optional message describing the response",
+        default=None
+    )
