cf46a9b49bf908bfb8fa6c46a0413149d9596089
Lara Filippone <lara.filippone@gmail.com>
2025-07-13 11:23:07 +0200

    feat: add auth_router in the app router

--- src/api/v1/v1_router.py
+++ src/api/v1/v1_router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from src.api.v1.endpoints.users import router as user_router
+from src.api.v1.endpoints.user import router as user_router
+from src.api.v1.endpoints.auth import router as auth_router
 
 
 v1_router = APIRouter()
 
 v1_router.include_router(user_router, prefix="/user")
+v1_router.include_router(auth_router)
