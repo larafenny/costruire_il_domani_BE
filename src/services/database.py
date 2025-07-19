cd87058f518304c104fcda70cccc0a61cf1d7cec
Lara Filippone <lara.filippone@gmail.com>
2025-07-19 13:17:04 +0200

    fix: create all tables in DB engine

--- src/services/database.py
+++ src/services/database.py
@@ -1,7 +1,14 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, SQLModel
 
 from src.core.settings import config
+
+from src.models.organization import Organization
+from src.models.file import File
+from src.models.user import User
 
 
 engine = create_engine(f"{config.DB_CONNECTION}://{config.DB_USERNAME}:{config.DB_PASSWORD}@{config.DB_HOST}:"
                        f"{config.DB_PORT}/{config.DB_DATABASE}")
+
+
+SQLModel.metadata.create_all(engine)
